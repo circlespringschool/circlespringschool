17ef37fbf6165c3bc4c494f474c2c44fcca3d5b7
RickMuchira <rickcharlesm@gmail.com>
2026-02-16 11:21:44 +0300

    fix the admin -decap -vercel configuration

--- api/auth/complete.js
+++ api/auth/complete.js
@@ -1,3 +1,9 @@
 const { createVercelCompleteHandler } = require('netlify-cms-oauth-provider-node');
 
-module.exports = createVercelCompleteHandler({}, { useEnv: true });
+module.exports = createVercelCompleteHandler({
+  origin: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'https://circlespringschool-red.vercel.app',
+  oauthProvider: 'github',
+  oauthClientId: process.env.OAUTH_GITHUB_CLIENT_ID,
+  oauthClientSecret: process.env.OAUTH_GITHUB_CLIENT_SECRET,
+  oauthScopes: 'repo,user'
+}, { useEnv: true });
